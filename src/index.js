ea9fa724f6402682af6d7f67a2a5e3c8a6067819
Emmanuelle Laguna <emmanuelle.laguna@gmail.com>
2022-12-12 15:39:58 +0000

    Update 13 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const addDays = (date, days) => {
+  date.setDate(date.getDate() + days);
+  return date;
+};
+
+const getDateWithoutWorkingDates = (date = new Date(), days = 0) => {
+  if ((days = 0 && !date.toString().includes(["SAT", "SUN"]))) {
+    return date;
+  } else if (date.toString().includes(["SAT", "SUN"])) {
+    const newDate = addDays(date, 1);
+    return getDateWithoutWorkingDates(newDate, days);
+  } else {
+    const newDate = addDays(date, 1);
+    const newDays = days - 1;
+    return getDateWithoutWorkingDates(newDate, newDays);
+  }
+};
+
+const today = new Date();
+const daysToAdd = 5;
+
+document.getElementById("app").innerHTML = `Get a date without weekends`;
+
+document.getElementById("date").innerHTML = `Date: ${today}`;
+
+document.getElementById("daysToAdd").innerHTML = `Days to add: ${daysToAdd}`;
+
+document.getElementById(
+  "result"
+).innerHTML = `Result: ${getDateWithoutWorkingDates(today, daysToAdd)}`;
